0e7ef43b179ec8c051b76c6d7fec2a61f0b2b731
rimrim990 <lim990718@naver.com>
2024-04-01 18:01:57 +0900

    feat: 로또 티켓 생성
    Co-authored-by: inthejim <inthejim24@gmail.com>

---
+++ src/main/java/LottoTicket.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LottoTicket {
+
+    private static final int TICKET_SIZE = 6;
+
+    private final List<LottoNumber> lottoNumbers;
+
+    public LottoTicket(List<Integer> numbers) {
+        validateNotDuplicated(numbers);
+        validateSize(numbers);
+
+        this.lottoNumbers = numbers.stream()
+            .map(LottoNumber::new)
+            .collect(Collectors.toList());
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != TICKET_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개이어야 합니다.");
+        }
+    }
+
+    private void validateNotDuplicated(List<Integer> numbers) {
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+
+        if (numbers.size() != distinctNumbers.size()) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
+}
