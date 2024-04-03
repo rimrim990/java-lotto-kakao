33c4b8c12f2a0b5ad605d17758b22fb543d67a33
rimrim990 <lim990718@naver.com>
2024-04-03 15:05:31 +0900

    feat: 당첨 수익률 계산 기능 구현
    Co-authored-by: inthejim <inthejim24@gmail.com>

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,22 +1,38 @@
 package domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LottoGame {
 
-    private final WinningLotto winningLotto;
+    private static final int LOTTO_PRICE = 1000;
+
     private final List<LottoTicket> lottoTickets;
+    private final Map<LottoPrice, Integer> lottoResult;
 
     public LottoGame(WinningLotto winningLotto, List<LottoTicket> lottoTickets) {
-        this.winningLotto = winningLotto;
         this.lottoTickets = lottoTickets;
+        this.lottoResult = calculateRank(winningLotto);
     }
 
-    public Map<LottoPrice, Integer> calculateRank() {
+    private Map<LottoPrice, Integer> calculateRank(WinningLotto winningLotto) {
         return lottoTickets.stream()
             .collect(Collectors.toMap(
                 winningLotto::calculatePrize, lottoTicket -> 1, Integer::sum));
+    }
+
+    public Map<LottoPrice, Integer> getRank() {
+        return Collections.unmodifiableMap(lottoResult);
+    }
+
+    public float calculateRevenue() {
+        int revenue = lottoResult.entrySet()
+            .stream()
+            .mapToInt(entry -> entry.getKey().getPrice() * entry.getValue())
+            .sum();
+
+        return (float) revenue / (LOTTO_PRICE * lottoTickets.size());
     }
 }
--- src/test/java/LottoGameTest.java
+++ src/test/java/LottoGameTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 
 import domain.LottoGame;
 import domain.LottoNumber;
@@ -25,9 +26,22 @@ class LottoGameTest {
         );
 
         LottoGame lottoGame = new LottoGame(winningLotto, lottoTickets);
-        Map<LottoPrice, Integer> result = lottoGame.calculateRank();
+        Map<LottoPrice, Integer> result = lottoGame.getRank();
 
         assertThat(result).containsEntry(LottoPrice.NOTHING, 1);
         assertThat(result).containsEntry(LottoPrice.FOURTH, 1);
+    }
+
+    @Test
+    void 로또번호_수익률() {
+        List<LottoTicket> lottoTickets = List.of(
+            new LottoTicket(List.of(11, 12, 13, 14, 15, 16)),
+            new LottoTicket(List.of(1, 2, 3, 7, 8, 9))
+        );
+
+        LottoGame lottoGame = new LottoGame(winningLotto, lottoTickets);
+        float result = lottoGame.calculateRevenue();
+
+        assertThat(result).isCloseTo(2.5f, within(0.5f));
     }
 }
