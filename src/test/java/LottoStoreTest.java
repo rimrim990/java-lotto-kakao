2d353a0d589278f93f266425e48b2d15b50ed951
rimrim990 <lim990718@naver.com>
2024-04-04 11:37:25 +0900

    test: 자동 로또 번호 발권 테스트

--- src/test/java/LottoStoreTest.java
+++ src/test/java/LottoStoreTest.java
@@ -1,7 +1,10 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.LottoStore;
+import domain.LottoTicket;
 import domain.Money;
+import domain.NumberGenerator;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +17,22 @@ public class LottoStoreTest {
 
         assertThat(lottoStore.getLottoCount()).isEqualTo(14);
         assertThat(lottoStore.getLottoTickets(new LottoNumberGenerator())).hasSize(14);
+    }
+
+    @Test
+    void 자동로또_발권() {
+        LottoStore lottoStore = new LottoStore(new Money(1_000));
+        List<LottoTicket> lottoTickets = lottoStore.getLottoTickets(new MockNumberGeneratr());
+
+        assertThat(lottoTickets).hasSize(1);
+        assertThat(lottoTickets.get(0).getLottoNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    private static class MockNumberGeneratr implements NumberGenerator {
+
+        @Override
+        public List<Integer> generateNumbers() {
+            return List.of(1, 2, 3, 4, 5, 6);
+        }
     }
 }
